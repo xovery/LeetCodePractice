ea1f500ce4b19bc8053fb8e3b0748864078a0c6a
chenming <chenming.hsu@toshibagcs.com>
2024-02-23 15:47:20 +0800

    update practice 1346

--- 1346.py
+++ 1346.py
@@ -1,12 +1,14 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         seen = set()
+        #seen = []
 
         for num in arr:
             if num * 2 in seen or num / 2 in seen:
                 return True
             else:
                 seen.add(num)
+                #seen.apend
 
         return False
 
